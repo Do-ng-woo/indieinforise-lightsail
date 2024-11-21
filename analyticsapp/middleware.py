a71950268e186e2543fb1e7bc90c207311d2757a
dongwooseo <2tiny2@naver.com>
2024-11-21 17:03:07 +0900

    애널리틱스 수정

--- analyticsapp/middleware.py
+++ analyticsapp/middleware.py
@@ -1,8 +1,5 @@
-# analyticsapp/middleware.py
-
 from .models import VisitorSession
 from django.utils import timezone
-from django.urls import resolve
 
 class VisitorSessionMiddleware:
     def __init__(self, get_response):
@@ -13,14 +10,27 @@ class VisitorSessionMiddleware:
         if not request.session.session_key:
             request.session.create()
 
-        # 세션 키가 유효한 경우에만 VisitorSession을 생성합니다.
         session_key = request.session.session_key
-        if session_key:
-            ip_address = request.META.get('REMOTE_ADDR', '')
-            user_agent = request.META.get('HTTP_USER_AGENT', '')
-            user_id = request.user.id if request.user.is_authenticated else None
 
-            # 기존 세션이 있는지 확인
+        # IP 주소 가져오기 (실제 클라이언트 IP 우선)
+        ip_address = request.META.get('HTTP_X_FORWARDED_FOR')
+        if ip_address:
+            ip_address = ip_address.split(',')[0].strip()
+        else:
+            ip_address = request.META.get('REMOTE_ADDR', '')
+
+        # 로드 밸런서 또는 봇 요청 필터링
+        ignored_user_agents = ["ELB-HealthChecker", "Googlebot", "Bingbot"]
+        ignored_ips = ["127.0.0.1"]  # 로드 밸런서 상태 점검 IP 추가
+        if any(ua in request.META.get("HTTP_USER_AGENT", "") for ua in ignored_user_agents) or ip_address in ignored_ips:
+            return self.get_response(request)
+
+        # User-Agent와 User 정보 가져오기
+        user_agent = request.META.get('HTTP_USER_AGENT', '')
+        user_id = request.user.id if request.user.is_authenticated else None
+
+        # VisitorSession 생성 또는 업데이트
+        if session_key:
             session, created = VisitorSession.objects.get_or_create(
                 session_key=session_key,
                 defaults={
@@ -32,8 +42,8 @@ class VisitorSessionMiddleware:
                 }
             )
 
-            # 기존 세션이면 페이지 방문 수 증가 및 종료 시간 업데이트
             if not created:
+                # 기존 세션이면 페이지뷰 증가 및 종료 시간 업데이트
                 session.page_views += 1
                 session.end_time = timezone.now()
                 session.save()
@@ -42,5 +52,6 @@ class VisitorSessionMiddleware:
                 session.end_time = timezone.now()
                 session.save()
 
+        # 다음 미들웨어/뷰 처리
         response = self.get_response(request)
         return response
