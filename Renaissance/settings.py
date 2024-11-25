34b4c6b170d1ef1c772ad120433153b788481e54
dongwooseo <2tiny2@naver.com>
2024-11-25 12:59:46 +0900

    페이스북로그인

--- Renaissance/settings.py
+++ Renaissance/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',  # 구글 로그인 추가
     'allauth.socialaccount.providers.naver',   # 네이버 로그인 추가
     'allauth.socialaccount.providers.kakao',
+    'allauth.socialaccount.providers.facebook',
     'accountapp',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -145,6 +146,18 @@ SOCIALACCOUNT_PROVIDERS = {
         'APP': {
             'client_id': env('KAKAO_REST_API_KEY'),
             'secret': '',
+            'key': ''
+        }
+    },
+    'facebook': {
+        'METHOD': 'oauth2',
+        'SCOPE': ['email', 'public_profile'],
+        'FIELDS': ['id', 'email', 'name', 'first_name', 'last_name'],
+        'VERIFIED_EMAIL': True,
+        'VERSION': 'v13.0',  # Facebook API 버전
+        'APP': {
+            'client_id': env('FACEBOOK_CLIENT_ID'),
+            'secret': env('FACEBOOK_CLIENT_SECRET'),
             'key': ''
         }
     }
